6b4024fce40ea672735af8bc5638ae4989f9680f
codelover <62229912+DedicatedDev@users.noreply.github.com>
2023-11-09 11:28:07 +0200

    Test/stg farm static (#50)
    * test: stg farm static contract
    
    * prod env
    
    ---------
    
    Co-authored-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/constants/networks/matic.ts
+++ src/constants/networks/matic.ts
@@ -64,6 +64,7 @@ const maticInfo: EVMNetworkInfo = {
     routers: '0xF9c2b5746c946EF883ab2660BbbB1f10A5bdeAb4',
     farms: ['0x7D5ba536ab244aAA1EA42aB88428847F25E3E676'],
     farmv2Quoter: '0x6AFeb9EDd6Cf44fA8E89b1eee28284e6dD7705C8',
+
     farmV2S: [
       '0xA70a1Ad0F91c52c79e177c5A18a7C9E1C4360e0e',
       '0x3D6AfE2fB73fFEd2E3dD00c501A174554e147a43',
@@ -76,6 +77,7 @@ const maticInfo: EVMNetworkInfo = {
       helper: '0x4E8419EFa0b0A149Dad77b689D37AF17f762f20A',
     },
     'farmV2.1S': [],
+
   },
   limitOrder: '*',
   averageBlockTimeInSeconds: 2.6,
